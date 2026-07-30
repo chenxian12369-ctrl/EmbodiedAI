8e88159b2b88945f8945dcff87c643f98871d937
Chen Xian <chenxian12369@gmail.com>
2026-07-30 21:30:24 +0800

    Day30 Learn NumPy vector operations

--- newrobot/numpy/test_numpy.py
+++ newrobot/numpy/test_numpy.py
@@ -21,3 +21,38 @@ a = np.array([
 print(a[0])
 print(a[1])
 print(a[0][1])
+
+import numpy as np
+
+a = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+print(a[1])
+
+print(a[:,1])
+
+print(a[:2,:2])
+
+print(a[:,-1])
+
+print(a[2])
+
+print(a[0:2])
+
+print(a[1:,1:])
+
+a = np.array([3,5])
+
+b = np.array([2,1])
+
+print(a+b)
+
+print(a-b)
+
+print(a*2)
+
+print(a/2)
+print([3,5]+[2,1])
