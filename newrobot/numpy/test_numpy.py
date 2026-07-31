18718922287bab8d10adedbd03504dacfe2e7783
Chen Xian <chenxian12369@gmail.com>
2026-07-31 18:56:51 +0800

    Day31 Learn NumPy broadcasting

--- newrobot/numpy/test_numpy.py
+++ newrobot/numpy/test_numpy.py
@@ -1,58 +1,75 @@
-import numpy as np
+# import numpy as np
 
-print("NumPy版本：", np.__version__)
+# print("NumPy版本：", np.__version__)
 
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
+# a = np.array([1, 2, 3])
+# b = np.array([4, 5, 6])
 
-print(a)
-print(b)
+# print(a)
+# print(b)
 
-print(a + b)
-print(a * b)
+# print(a + b)
+# print(a * b)
 
 
 
-a = np.array([
-    [1,2],
-    [3,4]
-])
+# a = np.array([
+#     [1,2],
+#     [3,4]
+# ])
 
-print(a[0])
-print(a[1])
-print(a[0][1])
+# print(a[0])
+# print(a[1])
+# print(a[0][1])
+
+# import numpy as np
+
+# a = np.array([
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ])
+
+# print(a[1])
+
+# print(a[:,1])
+
+# print(a[:2,:2])
+
+# print(a[:,-1])
+
+# print(a[2])
+
+# print(a[0:2])
+
+# print(a[1:,1:])
+
+# a = np.array([3,5])
+
+# b = np.array([2,1])
+
+# print(a+b)
+
+# print(a-b)
+
+# print(a*2)
+
+# print(a/2)
+# print([3,5]+[2,1])day30
 
 import numpy as np
 
 a = np.array([
     [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [4,5,6]
 ])
 
-print(a[1])
-
-print(a[:,1])
-
-print(a[:2,:2])
-
-print(a[:,-1])
-
-print(a[2])
-
-print(a[0:2])
-
-print(a[1:,1:])
-
-a = np.array([3,5])
-
-b = np.array([2,1])
+print(a)
+print(a + 10)
+print(a * 2)
+b = np.array([10,20,30])
 
 print(a+b)
-
-print(a-b)
-
-print(a*2)
+print(a-1)
 
 print(a/2)
-print([3,5]+[2,1])
